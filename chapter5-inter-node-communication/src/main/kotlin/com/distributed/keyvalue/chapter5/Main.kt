75a6eb7314f746527885307376f712c0aac1d9ae
김선우 <seonwoo960000@toss.im>
2025-08-03 18:14:01 +0900

    Remove unnecessary code

--- chapter5-inter-node-communication/src/main/kotlin/com/distributed/keyvalue/chapter5/Main.kt
+++ chapter5-inter-node-communication/src/main/kotlin/com/distributed/keyvalue/chapter5/Main.kt
@@ -1,41 +1,4 @@
 package com.distributed.keyvalue.chapter5
 
-import com.distributed.keyvalue.chapter1.WriteAheadLog
-import com.distributed.keyvalue.chapter2.KeyRangePartitioner
-import com.distributed.keyvalue.chapter3.LamportClock
-import com.distributed.keyvalue.chapter4.LeaseManager
-
 fun main() {
-    println("Chapter 5: Inter-Node Communication")
-    
-    // Using components from previous chapters
-    val wal = WriteAheadLog()
-    val partitioner = KeyRangePartitioner(wal)
-    val clock = LamportClock()
-    val leaseManager = LeaseManager(clock)
-    
-    val partition = partitioner.assignToPartition("node-1")
-    val lease = leaseManager.grantLease("resource-1", "node-1", 60000)
-    
-    println("Node assigned to partition $partition with lease timestamp ${lease.timestamp}")
-    
-    // Example implementation will go here
-    val channel = SingleSocketChannel("node-2")
-    channel.send(BatchRequest(listOf("get key1", "set key2 value2")))
 }
-
-// Example class for Single Socket Channel pattern
-class SingleSocketChannel(private val peerId: String) {
-    fun send(request: BatchRequest) {
-        println("Sending batch request to $peerId with ${request.commands.size} commands")
-        // Implementation details would go here
-    }
-    
-    fun receive(): String {
-        // Implementation details would go here
-        return "Response from $peerId"
-    }
-}
-
-// Example class for Batch Requesting pattern
-data class BatchRequest(val commands: List<String>)
