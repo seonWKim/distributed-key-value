977f50c640e72641604a5d8aec80936df5f360e7
김선우 <seonwoo960000@toss.im>
2025-08-04 02:55:10 +0900

    Update scripts

--- scripts/start-client.sh
+++ scripts/start-client.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# sh scripts/start-client.sh --port=20000
+
 set -e
 
 # === Configuration ===
 JAR_PATH="client/build/libs/client-all.jar"
+
+# === Parse & Show Arguments ===
+echo "Starting client with arguments: $@"
 
 # === Build ===
 echo "Cleaning and building client..."
@@ -11,5 +16,5 @@ echo "Cleaning and building client..."
 ./gradlew client:build
 
 # === Start Client ===
-echo "Starting client..."
-java -jar $JAR_PATH "$@"
+echo "Launching client..."
+java -jar "$JAR_PATH" "$@"
